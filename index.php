2eb4c34312b073f7813ca97ea654897550a39fb6
James Connor <jamesegconnor@gmail.com>
2020-10-19 18:36:30 -0400

    Updated Homepage
    - Homepage needed updating for Google OAuth compliance
    - Modified index.php: Added new content expanding on what YouCap really is.
    - Modified homepage.css: Added new styles for additional content.
    - Modified stylesheet.css: Fixed a style bug with the navigation icon being too small.
    - Added a number of image files.
    - Added captions.png, captions.svg: An icon for the homepage.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <?php include($_SERVER["DOCUMENT_ROOT"] . '/php/meta.php'); ?>
         
         <!-- Imports -->
-        <link href="https://fonts.googleapis.com/css2?family=Alata&family=Roboto+Condensed&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Alata&family=Roboto+Condensed&family=Noto+Sans&display=swap" rel="stylesheet">
         
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="/css/reset.css">
@@ -44,6 +44,33 @@
                     <p>Learn more about the YouCap service, including our standards and additional ways to contribute.</p>
                 </div>
             </a>
+            <div class="span left">
+                <img src="images/illustrations/drawkit-notebook-man-colour-800px.png" width="70%">
+            </div>
+            <div class="span right">
+                <h2>YouCap is all about creating.</h2>
+                <p>Our goal is to provide captions for as many YouTube videos as possible, in as many languages as possible. To that end, we offer a caption studio to create new captions or review those created by others.</p>
+                <a href="/pages/create"><p>Start Creating</p></a>
+            </div>
+            <div class="sep span2"></div>
+            <div class="span right">
+                <img src="images/illustrations/mobile-post-colour-800px.png" width="70%">
+            </div>
+            <div class="span left">
+                <h2>Using YouCap's captions is as simple as a single browser extension.</h2>
+                <p>YouCap's website works in conjunction with a browser extension to offer a seamless experience for users. Simply install it on your device and navigate to YouTube. From there you'll have a nearly identical experience to YouTube's own caption system.</p>
+                <a href="/pages/organization/extension"><p>Get the Extension</p></a>
+            </div>
+            <div class="sep span2"></div>
+            <div class="span left">
+                <img src="images/illustrations/unlock-800px.png" width="70%">
+            </div>
+            <div class="span right">
+                <h2>Privacy and security is our number one priority. We take it seriously, because we know it's serious to you.</h2>
+                <p>In an increasingly digital world, user information is constantly risking exposure. That's why YouCap operates off of a least-privilege model. That's to say, we use and store as little data as necessary to operate our platform. We also encourage security researchers to report any vulnerabilities they might find.</p>
+                <a href="/pages/organization/privacy"><p>Read our Privacy Policy</p></a>
+                <a href="/pages/organization/security"><p>Visit the Security Center</p></a>
+            </div>
         </div>
         
         <?php include($_SERVER["DOCUMENT_ROOT"] . "/php/footer.php"); ?>
