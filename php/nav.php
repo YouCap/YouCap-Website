747732abb24605858682cf5e8e73cc3accd38d19
James Connor <jamesegconnor@gmail.com>
2020-11-11 14:40:31 -0500

    Google Updates
    - Updated some files to conform to Google's styling guidelines and search console requirements.
    - Modified .htaccess: Removed access to the vendor file, cause there's no need
    - Modified robots.txt: Allowed access by the Google and Twitter bots
    - Modified homepage.css, stylesheet.css, nav.php: Navbar styling to fit Google's sign-in button with a longer title.

--- php/nav.php
+++ php/nav.php
@@ -6,7 +6,7 @@
             </div>
         </div>
     </a>
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="light" data-longtitle="true"></div>
 </nav>
 <script src="https://apis.google.com/js/platform.js"></script>
 <script src="/js/google-utils.js"></script>
